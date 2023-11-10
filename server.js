f49aea0fb213718ee033668b6c0677175a447db9
ProfJordan <Jordan.Halliday@slcc.edu>
2023-11-10 14:13:58 -0700

    User Registration

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please log in',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -59,7 +60,29 @@ myDB(async client => {
     res.render('profile', { username: req.user.username }); 
   });
 
-
+  app.route('/register').post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) next(err);
+      else if (user) res.redirect('/');
+      else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) res.redirect('/');
+            else next(null, doc.ops[0]);
+          }
+        )
+      }
+    })
+  },
+  passport.authenticate('local', { failureRedirect: '/' }),
+  (req, res, next) => {
+    res.redirect('/profile');
+  }
+  );
+  
   //logout route
   app.route('/logout')
   .get((req, res) => {
