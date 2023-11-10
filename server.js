5e314cb7600eb0859e11a53bc7e667cae7a15a42
ProfJordan <Jordan.Halliday@slcc.edu>
2023-11-10 15:24:03 -0700

    Password Encryption

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const URI = process.env.MONGO_URI;
 const LocalStrategy = require('passport-local');
+const { hash } = require('bcrypt');
 
 const app = express();
 
@@ -29,6 +30,8 @@ fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const hash = bcrypt.hashSync(req.body.password, 12);
 
 //app logic
 myDB(async client => {
@@ -67,7 +70,7 @@ myDB(async client => {
       else {
         myDataBase.insertOne({
           username: req.body.username,
-          password: req.body.password
+          password: hash
         },
           (err, doc) => {
             if (err) res.redirect('/');
@@ -101,7 +104,9 @@ app.use((req, res, next) => {
       console.log(`User ${username} attempted to log in.`);
       if (err) return done(err);
       if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) { 
+        return done(null, false);
+      };
       return done(null, user);
     });
   }));
