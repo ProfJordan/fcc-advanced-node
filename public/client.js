80c55dc6f1e3f9d66df135305a234bc06ddbb4c8
ProfJordan <Jordan.Halliday@slcc.edu>
2023-11-14 20:20:08 -0700

    Send/Display chat messages

--- public/client.js
+++ public/client.js
@@ -11,9 +11,16 @@ $(document).ready(function() {
     $('#messages').append($('<li>').html('<b>' + message + '</b>'));
   });
 
+socket.on('chat message', data => {
+  console.log('socket.on 1')
+  $('#messages').append($('<li>').text(`${data.username}: ${data.message}`));
+});
+
   // Form submittion with new message in field with id 'm'
   $('form').submit(function() {
     var messageToSend = $('#m').val();
+    // Send message to server
+    socket.emit('chat message', messageToSend);
 
     $('#m').val('');
     return false; // prevent form submit from refreshing page
