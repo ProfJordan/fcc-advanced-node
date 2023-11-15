97479996d7d3d65c54697822f68c8a4e15206c5d
ProfJordan <Jordan.Halliday@slcc.edu>
2023-11-14 19:59:14 -0700

    server cleanup

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set('views', './views/pug');
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  secret: 'secret',
+  secret: secret,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false },
@@ -39,58 +39,56 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-function onAuthorizeSuccess(data, accept) {
-  console.log('successful connection to socket.io');
-
-  accept(null, true);
-}
-
 io.use(
   passportSocketIo.authorize({
     cookieParser: cookieParser,
     key: 'express.sid',
     secret: process.env.SESSION_SECRET,
+    secret: secret,
     store: store,
     success: onAuthorizeSuccess,
     fail: onAuthorizeFail
   })
 );
 
-//app logic
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
+
   routes(app, myDataBase);
   auth(app, myDataBase);
 
   let currentUsers = 0;
-  //socket.io logic
-io.on('connection', socket => {
-  ++currentUsers;
-  io.emit('user count', currentUsers);
-  console.log('A user has connected');
-
-  socket.on('disconnect', () => {
-    console.log('A user has disconnected');
-    --currentUsers;
+  io.on('connection', (socket) => {
+    ++currentUsers;
     io.emit('user count', currentUsers);
-  });
-});
+    console.log('A user has connected');
 
-//error handling
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user count', currentUsers);
+    });
+  });
+  
 }).catch(e => {
   app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: `Unable to connect to database` });
+    res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
 
 function onAuthorizeFail(data, message, error, accept) {
   if (error) throw new Error(message);
   console.log('failed connection to socket.io:', message);
   accept(null, false);
 }
-
+  
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-  console.log('user ' + socket.request.user.username + ' connected');
 });
