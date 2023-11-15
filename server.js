98d3dd006b55bc21c0ac8febd85bceb7a091ae3a
ProfJordan <Jordan.Halliday@slcc.edu>
2023-11-14 19:37:18 -0700

    handle disconnect

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ io.on('connection', socket => {
   ++currentUsers;
   io.emit('user count', currentUsers);
   console.log('A user has connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected');
+    --currentUsers;
+    io.emit('user count', currentUsers);
+  });
 });
 
 //error handling
